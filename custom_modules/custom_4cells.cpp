c7083477edbe7aad45b54684ccc0026b7fd416cb
Randy Heiland <heiland@indiana.edu>
2020-01-15 20:00:40 -0500

    cleaned up

--- custom_modules/custom_4cells.cpp
+++ custom_modules/custom_4cells.cpp
@@ -73,45 +73,33 @@ Cell_Definition DCC;
 
 Cycle_Model csc_cycle_model;
 
-int previous_num_cells = -1;
-
 double prob_p = 0.5;
 double one_minus_prob_p = 0.5;
 
-// void differentiate_daughter_cell( Cell* pCell, Phenotype& phenotype, double dt )
-// {
-// 	// if ((pCell->type == 0) && (UniformRandom() < 0.5))
-// 	return;
-// }
-
-// 	phenotype.cycle.data.elapsed_time_in_phase += dt; 
 void check_cell_cycle_and_differentiate( Cell* pCell, Phenotype& phenotype, double dt )
 {
-	// if (PhysiCell_globals.current_time > 6.0)
-	// {
-		double elapsed_time = phenotype.cycle.data.elapsed_time_in_phase;
-		// if ((elapsed_time < 0.6) && (pCell->type == 0))
-		// phenotype.execute_cycle_phase_entry_function = true; 
-		// std::cout << __FUNCTION__ << ": pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
-		// if (phenotype.execute_cycle_phase_entry_function && elapsed_time < 0.6) 
-		if (elapsed_time < 0.1) 
+	double elapsed_time = phenotype.cycle.data.elapsed_time_in_phase;
+	// if ((elapsed_time < 0.6) && (pCell->type == 0))
+	// phenotype.execute_cycle_phase_entry_function = true; 
+	// std::cout << __FUNCTION__ << ": pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
+	// if (phenotype.execute_cycle_phase_entry_function && elapsed_time < 0.6) 
+	if (elapsed_time < 0.1) 
+	{
+		// std::cout << __FUNCTION__ << "pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
+		std::cout << "pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time;
+		std::cout << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
+		std::cout << ", flagged_for_division = " << phenotype.flagged_for_division << std::endl;
+
+		if (UniformRandom() <= one_minus_prob_p)
 		{
-			// std::cout << __FUNCTION__ << "pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
-			std::cout << "pCell->ID = "<< pCell->ID << ", elapsed_time_in_phase = " << elapsed_time;
-			std::cout << ", current_time = " << PhysiCell_globals.current_time<< std::endl;
-			std::cout << ", flagged_for_division = " << phenotype.flagged_for_division << std::endl;
-
-			if (UniformRandom() <= one_minus_prob_p)
-			{
-				// pCell->type = 1;
-				// if (pCell->type == 0)
-				std::cout << "*******  CSC --> DCC\n";
-				pCell->convert_to_cell_definition(DCC);
-			}
-
-			phenotype.execute_cycle_phase_entry_function = false;
+			// pCell->type = 1;
+			// if (pCell->type == 0)
+			std::cout << "*******  CSC --> DCC\n";
+			pCell->convert_to_cell_definition(DCC);
 		}
-	// }
+
+		phenotype.execute_cycle_phase_entry_function = false;
+	}
 }
 
 void create_cell_types( void )
@@ -128,18 +116,11 @@ void create_cell_types( void )
 	std::cout << "--------- prob_p = " << prob_p << std::endl;
 	std::cout << "--------- 1 - prob_p = " << one_minus_prob_p << std::endl;
 
-	// housekeeping 
-	
 	initialize_default_cell_definition();
-	// cell_defaults.phenotype.secretion.sync_to_microenvironment( &microenvironment ); 
 	
 	// set default cell cycle model 
-	// cell_defaults.functions.cycle_model = live; 
-	// vs.  ??
 	cell_defaults.phenotype.cycle.sync_to_cycle_model( live );   // rwh: can I override this later??
 	// set default_cell_functions; 
-	
-	// cell_defaults.functions.update_phenotype = update_cell_and_death_parameters_O2_based; 
 	
 	cell_defaults.functions.set_orientation = up_orientation; 
 	cell_defaults.phenotype.geometry.polarity = 1.0;
@@ -183,61 +164,13 @@ void create_cell_types( void )
 	CSC = cell_defaults; 
 	CSC.type = 0; 
 	CSC.name = "CSC"; 
-
-	// create_csc_cycle_model();
-	// CSC.phenotype.cycle.sync_to_cycle_model( csc_cycle_model );
-	
-	// // CSC.phenotype.cycle.sync_to_cycle_model( live ); 
-
-	// CSC.functions.cycle_model = csc_cycle_model;
-
 	CSC.functions.update_phenotype = check_cell_cycle_and_differentiate; 
 	
-	// csc_cycle_model = live;
-	// csc_cycle_model.code = PhysiCell_constants::custom_cycle_model;
-	// csc_cycle_model.name = "CSC cycle model";
-	// // csc_cycle_model.phases[0].entry_function = csc_differentiation_function;
-	// csc_cycle_model.phase_link(0,0).exit_function = csc_differentiation_function;
-	// CSC.functions.cycle_model = csc_cycle_model; 
-	// CSC.phenotype.cycle.data.transition_rate(0,0) = 0.00042; 	
-	
-	// // csc_cycle_model.display(std::cout);
-	
-	// CSC.phenotype.sync_to_functions( CSC.functions ); 
 
-	// rgb_model.phase_link(blue_index,red_index).exit_function = my_mutation_function;
-	// live.phases[0].entry_function = standard_live_phase_entry_function;
-	// CSC.phenotype.cycle.data.transition_rate(live_index,live_index) 
-	// CSC.phenotype.cycle.model.ph 
-	// int live_index = live.find_phase_index( PhysiCell_constants::live );
-	// live.phases[0].entry_function = standard_live_phase_entry_function;    // in create_live_model()
-	// CSC.phenotype.cycle.data.transition_rate(cycle_start_index,cycle_end_index) = 0.0; 	
-	
-	// make this cell type randomly motile, less adhesive, greater survival, 
-	// and less proliferative 
-	
 	DCC = cell_defaults; 
 	DCC.type = 1; 
 	DCC.name = "DCC"; 
 	
-	// make sure the new cell type has its own reference phenotype
-	
-	// DCC.parameters.pReference_live_phenotype = &( DCC.phenotype ); 
-
-	// DCC.phenotype.cycle.data.transition_rate(live_index,live_index) *= parameters.doubles( "DCC_relative_cycle_entry_rate" );
-	
-	// Set apoptosis to 
-	// DCC.phenotype.death.rates[apoptosis_model_index] = parameters.doubles( "DCC_apoptosis_rate" ); // 0.0; 
-	
-	// Set proliferation to 10% of other cells. 
-	// Alter the transition rate from G0G1 state to S state
-	// DCC.phenotype.cycle.data.transition_rate(live_index,live_index) *= parameters.doubles( "DCC_relative_cycle_entry_rate" );
-
-	// int cycle_start_index = live.find_phase_index( PhysiCell_constants::live ); 
-	// int cycle_end_index = live.find_phase_index( PhysiCell_constants::live ); 
-	// DCC.phenotype.cycle.data.transition_rate( cycle_start_index , cycle_end_index ) = 0.0; 
-	// DCC.phenotype.cycle.sync_to_cycle_model( live );  // repeating the default
-
 	return; 
 }
 
@@ -253,29 +186,6 @@ void setup_microenvironment( void )
 	return; 
 }
 
-void setup_tissue_more( void )
-{
-	double pct = 0.7;
-	double length_x = (microenvironment.mesh.bounding_box[3] - microenvironment.mesh.bounding_box[0]) * pct; 
-	double length_y = (microenvironment.mesh.bounding_box[4] - microenvironment.mesh.bounding_box[1]) * pct; 
-
-	double offset = (microenvironment.mesh.bounding_box[3] - microenvironment.mesh.bounding_box[0]) * (1.0-pct)/2.0; 
-		
-	Cell* pC;
-	
-	// int number_of_infected_cells = parameters.ints( "number_of_infected_cells" ); 
-
-	for( int n = 0 ; n < 30; n++ )
-	{
-		pC = create_cell(CSC); 
-		double x = microenvironment.mesh.bounding_box[0] + offset + UniformRandom() * length_x; 
-		double y = microenvironment.mesh.bounding_box[1] + offset + UniformRandom() * length_y; 
-		pC->assign_position( x,y, 0.0 );
-		// pC->phenotype.molecular.internalized_total_substrates[ nVirus ] = 1; 
-	}
-}
-
-// void setup_tissue_orig( void )
 void setup_tissue( void )
 {
 	// create some cells near the origin
